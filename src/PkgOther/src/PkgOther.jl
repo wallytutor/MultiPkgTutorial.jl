3e88322ee35329f04fa692eb671cbb7a56def202
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-11-01 20:02:02 +0100

    Integrated changes

--- src/PkgOther/src/PkgOther.jl
+++ src/PkgOther/src/PkgOther.jl
@@ -4,7 +4,8 @@ using PkgCore
 
 function __init__()
     @info "Starting PkgOther"
-    @info "mean = $(PkgCore.coremean([1, 2, 3]))"
+    @info "mean ... $(PkgCore.coremean([1, 2, 3]))"
+    @info "std .... $(PkgCore.corestd([1, 2, 3]))"
 end
 
 end # module PkgOther
